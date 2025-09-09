e6cb136e1c718fe482cdf775178f63d4935c05a4
Wheeler <wheelera@CSGP.EDU@CSC-20.CSGP.EDU>
2025-09-09 09:36:07 -0600

    first 2 layers

---
+++ main.py
@@ -0,0 +1,31 @@
+def lifeDecisions():
+    print("suh twin")
+    print("finna make yo life decisions jit")
+
+    q1 = input("Will you [1], Hop on clash, or [2], take a shower\n")
+    if q1 == "1":
+        print("Good choice slime")
+        print("You have 10k trophies, so everyone respects you more now")
+
+        q2a = input("Now, will you [1], go to school on time, or [2], skip your 1st class\n")
+        if q2a == "1":
+            print("BOOOOORRIIINGG, you a loser fr fr but aight")
+            print("You get to your 1st class (on time) and start learning stuff")
+        elif q2a == "2":
+            print("ts aura")
+            print("You go on a side quest to Wendy's and get to school 30 minutes late")
+    elif q1 == "2":
+        print("Okay nerd, you smell aight ig")
+        print("You lowk chopped tho so it didn't matter")
+
+        q2b = input("So will you [1], go to school like the nerd you are, or [2], skip and scroll reels all day\n")
+        if q2b == "1":
+            print("Bruh this why u get bullied gang")
+            print("You go to school and start learning")
+        elif q2b == "2":
+            print("ts the CORRECT choice")
+            print("You discover the meaning of life")
+    else:
+        print("That ain no option gang")
+
+lifeDecisions()
